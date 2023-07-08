d41a3e90c35a3e61788c09e0f9b89a5e70e07ae2
HackbartPR <cgharckbart@gmail.com>
2023-07-08 15:21:03 -0300

    first commit

---
+++ Alura - Criando uma Web API/Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using Alura___Criando_uma_Web_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura___Criando_uma_Web_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> ListarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? BuscarFilme(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
+    }
+}
---
+++ Alura - Criando uma Web API/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alura___Criando_uma_Web_API.Models;
+
+public class Filme
+{    
+    public int Id { get; set; }
+
+    [Required (ErrorMessage = "O campo Título não pode ser nulo")]
+    public string Titulo { get; set; }
+    
+    [Required(ErrorMessage = "O campo Gênero não pode ser nulo")]
+    [MaxLength (50, ErrorMessage = "Tamanho máximo do campo Gênero é de 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "O campo Duração não pode ser nulo")]
+    [Range (70, 600, ErrorMessage = "Duração deve ser maior que 70 min e menor que 600 minutos")]
+    public int Duracao { get; set; }
+
+}
